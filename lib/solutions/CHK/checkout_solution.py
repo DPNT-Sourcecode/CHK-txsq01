bb0ac29242d53324e86ae8087af349f9055761d1
sbx_user1051 <sbx_user1051@169.254.13.49>
2025-03-17 01:26:21 +0000

    Mon Mar 17 01:26:21 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,9 +3,15 @@ def checkout(skus):
         return -1
     
     # to keep track of the prices of each SKU
-    prices = { 'A': 50, 'B': 30,'C': 20, 'D': 15,'E': 40,'F': 10 }
+    prices = { 'A': 50, 'B': 30,'C': 20, 'D': 15,'E': 40,'F': 10,
+              'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0,
+               'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0,
+               'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'X':0}}
     # hold number of occurances for each product
-    counts = { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0 }
+    counts = { 'A': 0, 'B': 0, 'C': 0, 'D': 0, 'E': 0, 'F': 0,
+               'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0,
+               'M': 0, 'N': 0, 'O': 0, 'P': 0, 'Q': 0, 'R': 0,
+               'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'X':0}
     # total cost
     total = 0
 
