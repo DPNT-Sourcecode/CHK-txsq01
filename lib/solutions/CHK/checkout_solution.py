7b582ff9d675e6f0aa4db4086d63288d65edca83
sbx_user1051 <sbx_user1051@169.254.13.49>
2025-03-17 00:32:50 +0000

    Mon Mar 17 00:32:50 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,5 +1,29 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    prices = {}
+
+    if not isinstance(skus, str):
+        return -1
+    
+    prices = {
+        'A': 50,
+        'B': 30,
+        'C': 20,
+        'D': 15
+    }
+
+    counts = {
+        'A': 0,
+        'B': 0,
+        'C': 0,
+        'D': 0
+    }
+
+    total = 0
+
+    for char in skus:
+        if char not in prices:
+            return -1
+        counts[c]
+
 
