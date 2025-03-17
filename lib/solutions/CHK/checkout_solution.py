e568df2002a8c70b00c6cf552aa73579e51ad47d
sbx_user1051 <sbx_user1051@169.254.13.49>
2025-03-17 00:38:50 +0000

    Mon Mar 17 00:38:50 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,6 +5,7 @@ def checkout(skus):
     if not isinstance(skus, str):
         return -1
     
+    # to keep track of the prices of each SKU
     prices = {
         'A': 50,
         'B': 30,
@@ -12,6 +13,7 @@ def checkout(skus):
         'D': 15
     }
 
+    # hold number of occurances for each product
     counts = {
         'A': 0,
         'B': 0,
@@ -19,11 +21,15 @@ def checkout(skus):
         'D': 0
     }
 
+    # total cost
     total = 0
 
+    # iterate through each char of the string 'skus'
     for char in skus:
+        # if the char is not from the valid skus, return 1
         if char not in prices:
             return -1
+        # otherwise 
         counts[char] += 1
 
     aCount = counts['A']
@@ -31,7 +37,13 @@ def checkout(skus):
     total += (aCount % 3) * prices['A']
 
     bCount = counts['B']
-    total += (bCount//2) * 130
-    total += (bCount % 3) * prices['B']
+    total += (bCount//2) * 45
+    total += (bCount % 2) * prices['B']
+
+    cCount = counts['C']
+    total += cCount * prices['C']
+
+    dCount = counts['D']
+    total += dCount * prices['D']
 
 
